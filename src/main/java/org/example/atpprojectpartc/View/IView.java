c8744ced786e5f9cb87253b22cacd97a3921350b
shiraEshcoli <eshcoli@post.bgu.ac.il>
2026-06-19 16:16:19 +0300

    change

--- src/main/java/org/example/atpprojectpartc/View/IView.java
+++ src/main/java/org/example/atpprojectpartc/View/IView.java
@@ -1,4 +1,19 @@
 package org.example.atpprojectpartc.View;
 
+import algorithms.mazeGenerators.Maze;
+import algorithms.mazeGenerators.Position;
+import algorithms.search.Solution;
+
 public interface IView {
+    void displayMaze(Maze maze);
+
+    void displayCharacterPosition(Position position);
+
+    void displaySolution(Solution solution);
+
+    void showError(String message);
+
+    void showWinMessage();
+
+    void setControlsEnabled(boolean enabled);
 }
